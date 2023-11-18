19edbd55785a99678749e40475a979d3afc58308
tiago canuto <tiagocanutogames@gmail.com>
2023-11-18 18:18:15 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function()         
+        {                                                    
+
+            $('#telefone').mask('(00) 00000-0000',    
+            {
+                placeholder: '(11) 98880-7965'  
+            })                                
+        })
